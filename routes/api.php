3841b706251033484652925de2f60d5d65775950
wzc <515819056@qq.com>
2018-12-15 14:10:58 +0800

    添加用户-发送验证码接口

--- routes/api.php
+++ routes/api.php
@@ -18,10 +18,13 @@ use Illuminate\Http\Request;
 //});
 $api = app('Dingo\Api\Routing\Router');
 
-$api->version('v1',function ($api){
+$api->version('v1',[
+        'namespace' => 'App\Http\Controllers\Api'
+    ],function ($api){
     $api->get('version', function() {
         return response('this is version v1');
     });
+    $api->post('verificationCodes','VerificationCodesController@store')->name('api.verificationCodes.store');
 });
 $api->version('v2',function ($api){
     $api->get('version', function() {
